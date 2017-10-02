137371a847d8fbf3e0b09328ed79924f633fb60f
RyanChung851 <ryan.chung851@gmail.com>
2017-10-02 17:56:16 -0400

    Update cam.py

--- cam.py
+++ cam.py
@@ -6,10 +6,10 @@ import os
     This program will only run on a raspberry pi that has motion package and odeke-em/drive Filesystem in Userspace installed.
     Credits to odeke-em/drive: https://github.com/odeke-em/drive
     This program will only run when motion is detected through a functional USB camera connected to the raspberry pi.
+    
 """
 
 """
-
     The main function will push the gdrive/Video folder content to the google drive account that is connected to the
     fuse filesystem.
 
@@ -17,7 +17,6 @@ import os
 
     If the current directory is /home/pi/gdrive/Video then the function will push all recorded video footage to the
     google drive.
-
 
 """
 
